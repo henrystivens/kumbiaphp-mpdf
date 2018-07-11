ea7526450eff33a13152e6b5b431a6ed0f79d8cc
Henry Stivens Adarme <henry.stivens@gmail.com>
2018-07-10 20:52:33 -0500

    Add views example2 and example3

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -1,4 +1,8 @@
 <?php
+// Require composer autoload
+require_once APP_PATH . '../../vendor/autoload.php';
+
+use Mpdf\Mpdf;
 
 /**
  * Controller por defecto si no se usa el routes
@@ -9,6 +13,34 @@ class IndexController extends AppController
 
     public function index()
     {
+        
+    }
 
+    public function example1()
+    {
+        //Importante: Sin vista y sin tamplate
+        View::select(null, null);
+        // Crea una instancia de la clase y le pasa el directorio default/app/temp/
+        $mpdf = new Mpdf(['tempDir' => APP_PATH . '/temp']);
+        // Escribe algo de contenido HTML:
+        $mpdf->WriteHTML('¡Hola KumbiaPHP!');
+        // Envia un archivo PDF directamente al navegador
+        $mpdf->Output();
+    }
+
+    public function example2()
+    {
+        //Importante: Sin vista y sin tamplate
+        View::select(null, null);
+        //Llama al ejemplo 2
+        PdfHtml::example2();
+    }
+
+    public function example3($name = 'KumbiaPHP')
+    {
+        //Importante: Sin vista y sin tamplate
+        View::select(null, null);
+        //Llama al ejemplo 3, recibe como parámetro el nombre por GET
+        PdfHtml::example3($name);
     }
 }
